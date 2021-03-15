c886d115d4d6644904b4071bda8892c65ccbe026
jorboare <jordi.b.arevalo@gmail.com>
2021-03-15 13:02:57 +0100

    homepage and about

---
+++ dist/js/main.js
@@ -0,0 +1,30 @@
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('nav')
+const menuNav = document.querySelector('.menu-nav')
+const navItems = document.querySelectorAll('.menu-nav__item')
+
+
+let showMenu = false;
+console.log(showMenu)
+
+menuBtn.addEventListener('click', toggleMenu)
+
+function toggleMenu() {
+    if (!showMenu) {
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        navItems.forEach(elm => elm.classList.add('open'))
+        showMenu = true;
+
+    } else {
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        navItems.forEach(elm => elm.classList.remove('open'))
+
+        showMenu = false
+
+    }
+}
